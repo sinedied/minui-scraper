42128d677d3d501c5dc346436ddc61210ab1cfcc
sinedied <noda@free.fr>
2025-02-23 22:01:10 +0100

    feat: wip implmentation

--- bin/mscraper.js
+++ bin/mscraper.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import process from 'node:process';
-import { run } from '../src/cli.js';
+import {run} from '../lib/cli.js';
 
 run(process.argv.slice(2));
---
+++ lib/libretro.d.ts
@@ -0,0 +1,2 @@
+export declare function getMachine(file: string): Promise<void>;
+export declare function isRomFolder(folderName: string): boolean;
