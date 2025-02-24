31f35a02fc324a21b538d58a00069db01122ab5b
sinedied <noda@free.fr>
2025-02-24 20:20:41 +0100

    fix: skip unreadable images

--- src/image.ts
+++ src/image.ts
@@ -8,9 +8,13 @@ export type Size = {
 };
 
 export async function resizeImageTo(url: string, destination: string, size?: Size) {
-  const width = size?.width ?? 250;
-  const height = size?.height;
-  const image = await Jimp.read(url);
-  await mkdir(path.dirname(destination), { recursive: true });
-  await image.resize({ w: width, h: height }).write(destination as `${string}.${string}`);
+  try {
+    const width = size?.width ?? 250;
+    const height = size?.height;
+    const image = await Jimp.read(url);
+    await mkdir(path.dirname(destination), { recursive: true });
+    await image.resize({ w: width, h: height }).write(destination as `${string}.${string}`);
+  } catch (error: any) {
+    console.error(`Failed to downloading art from "${url}": ${error.message}`);
+  }
 }
