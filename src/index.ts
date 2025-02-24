90dd068408646898abcef396fc983cde7e7ba881
sinedied <noda@free.fr>
2025-02-24 18:23:51 +0100

    chore: add lib exports

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './cli.js';
+export * from './libretro.js';
+export * from './image.js';
+export * from './matcher.js';
+export * from './ollama.js';
+export * from './options.js';
+export * from './stats.js';
