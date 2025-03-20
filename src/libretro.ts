a76d9d19a6841c9b1b604cd9b9d047f2ee50db8b
Ben Wadsworth <ben.wadsworth@certara.com>
2025-03-20 13:40:34 +0000

    reorder logic

--- src/libretro.ts
+++ src/libretro.ts
@@ -163,13 +163,13 @@ export async function findArtUrl(
   match = await findMatch(strippedName);
   if (match) return match;
 
-  // Try searching after removing substitles using '- '
-  strippedName = strippedName.split('- ')[0].trim();
+  // Try searching after removing substitles using ': '
+  strippedName = strippedName.split(': ')[0].trim();
   match = await findMatch(strippedName);
   if (match) return match;
 
-  // Try searching after removing substitles using ': '
-  strippedName = strippedName.split(': ')[0].trim();
+  // Try searching after removing substitles using '- '
+  strippedName = strippedName.split('- ')[0].trim();
   match = await findMatch(strippedName);
   if (match) return match;
 
